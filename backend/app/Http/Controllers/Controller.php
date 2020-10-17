b41804526cd2be31b26b912bea0db4bcd6557f55
Razvan <razvan.rauta@gmail.com>
2020-10-17 19:34:05 +0300

    Added Laravel Sanctum and Swagger

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -7,6 +7,20 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Amazing Laravel Api",
+ *      description="Not enough words to describe it",
+ *      @OA\Contact(
+ *          email="razvan@rrazvan.dev"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
