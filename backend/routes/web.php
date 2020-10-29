9cd9692afe12f53845ba8ae08612cfc963f46ec0
Razvan Rauta <rrauta@klika-tech.com>
2020-10-29 13:36:21 +0300

    Redirect from / to adverts

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Advert\AdvertController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return response()->json(['Nothing to see here']);
+    return redirect()->action([AdvertController::class, 'index']);
 });
