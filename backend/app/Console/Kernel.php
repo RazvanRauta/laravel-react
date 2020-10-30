a1ab20b8855c302be29cca6fcc965bc5d55dbf74
Razvan Rauta <razvan.rauta@gmail.com>
2020-10-30 11:07:57 +0300

    Append crontab logs to docker logs

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        $schedule->command('rr:parse-web-page')->everyTenMinutes();
+        $schedule->command('rr:parse-web-page')->everyTenMinutes()->appendOutputTo('/var/log/laravel-scheduler.log');
     }
 
     /**
